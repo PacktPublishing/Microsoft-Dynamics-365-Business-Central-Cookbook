a45a129de29b62f83a4b344b84341cde9d9a2b09
MikeGlue <mikeglue@live.ca>
2019-07-04 23:10:30 -0400

    ch8 files

---
+++ ch8/6-create-a-release-pipeline/Create container.ps1
@@ -0,0 +1,19 @@
+﻿if (Test-NavContainer -containerName $(containerName))
+{
+  Remove-NavContainer -containerName $(containerName)
+}
+
+New-NavContainer -containerName $(containerName) `
+  -accept_eula `
+  -accept_outdated `
+  -auth NavUserPassword `
+  -Credential (New-Object pscredential $(containerUserName), (ConvertTo-SecureString -String $(containerPassword) -AsPlainText -Force)) `
+  -doNotExportObjectsToText `
+  -imageName $(imageName) `
+  -alwaysPull `
+  -shortcuts CommonStartMenu `
+  -restart always `
+  -updateHosts `
+  -useBestContainerOS `
+  -assignPremiumPlan `
+  -additionalParameters @("--volume ""$($env:Agent_HomeDirectory):C:\Agent""")
