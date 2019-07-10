773fe834eed587eb1e81d9251dc93d0b24dff68a
MikeGlue <mikeglue@live.ca>
2019-07-09 23:31:22 -0400

    ch8 files

---
+++ ch8/6-create-a-release-pipeline/scripts/Install BC Application.ps1
@@ -0,0 +1,3 @@
+﻿Get-ChildItem -Path (Join-Path -Path $env:System_ArtifactsDirectory -ChildPath ($env:Release_PrimaryArtifactSourceAlias + '\televisionShowApp')) -Filter '*.app' | ForEach-Object {
+  Publish-NavContainerApp -containerName $env:containerName -appFile $_.FullName -skipVerification -sync -install
+}
