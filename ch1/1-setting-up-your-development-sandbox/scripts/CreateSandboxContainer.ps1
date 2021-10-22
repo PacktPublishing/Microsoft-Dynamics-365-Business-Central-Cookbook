f1da1866f0b0e477e15c39c524d408e6dbda3227
Mike Glue <mikeglue@live.ca>
2021-10-22 17:06:24 -0400

    update container script to support BcContainerHelper artifacts

--- ch1/1-setting-up-your-development-sandbox/scripts/CreateSandboxContainer.ps1
+++ ch1/1-setting-up-your-development-sandbox/scripts/CreateSandboxContainer.ps1
@@ -1,4 +1,4 @@
-﻿install-module navcontainerhelper -force
+﻿Install-Module -Name BcContainerHelper -Force
 
 # set to $true to accept the eula (https://go.microsoft.com/fwlink/?linkid=861843)
 $accept_eula = $false
@@ -6,8 +6,11 @@ $accept_eula = $false
 # set the name of your container (must be 15 characters or less)
 $containername = ''
 
-# set image to use to create container (see here for available images: https://hub.docker.com/_/microsoft-businesscentral-sandbox﻿)
-$bcDockerImage = 'mcr.microsoft.com/businesscentral/sandbox:us'
+# set the localization to use (NA = North America, W1 = Worldwide, etc.)
+$country = 'NA'
+
+# set the container isolation mode
+$isolation = 'process' # (can use 'hyperv' if 'process' isolation fails)
 
 # the user you use to login to the Business Central client (is a SUPER user)
 $userName = "admin"
@@ -16,19 +19,24 @@ $password = ConvertTo-SecureString -String "Pass@word1" -AsPlainText -Force
 # set the secure Url to your Business Central license file (leave blank to use the demo license)
 $licenseFileUri = ''
 
+# set the DNS address to use from within the container
+$dnsAddress = '8.8.8.8'
+
 $credential = New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList $userName, $password
 
-New-NavContainer -accept_eula:$accept_eula `
-                 -containername $containername `
-                 -auth UserPassword `
-                 -Credential $credential `
-                 -licenseFile $licenseFileUri `
-                 -includeCSide `
-                 -alwaysPull `
-                 -doNotExportObjectsToText `
-                 -usessl:$false `
-                 -updateHosts `
-                 -assignPremiumPlan `
-                 -shortcuts Desktop `
-                 -imageName $bcdockerimage `
-                 -useBestContainerOS
+$artifactUrl = Get-BCArtifactUrl -type OnPrem -country $country -version '13'
+
+New-BCContainer -accept_eula:$accept_eula `
+                -accept_outdated `
+                -containername $containername `
+                -artifactUrl $artifactUrl `
+                -auth UserPassword `
+                -Credential $credential `
+                -licenseFile $licenseFileUri `
+                -updateHosts `
+                -dns $dnsAddress `
+                -assignPremiumPlan `
+                -shortcuts Desktop `
+                -doNotExportObjectsToText `
+                -includeCSide `
+                -isolation $isolation
